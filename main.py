240a22fe118308b826722b083a527e7b37195b8a
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-03 18:11:53 +0200

    Push -- Initial changes on file for reading input filepath and some checks on this one

--- main.py
+++ main.py
@@ -3,14 +3,27 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import os
+from os import path
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def inputFile():
+    val = input("Enter the full path of the file to convert: ")
+    return val
 
+
+def main(name):
+    # Ask for the full path of the file to convert.
+    filepath = inputFile()
+    if os.path.exists(filepath):
+        if os.path.isfile(filepath):
+            print("File " + str(filepath) + " is existing")
+        else:
+            print("File " + str(filepath) + " is NOT existing")
+    else:
+        print("Specified filepath is not OK")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
