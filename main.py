e8bc1eb8e78fbc5233dd36d57d0215b81ddf82fa
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-04 17:18:10 +0200

    Push -- function "inputFile" will manage the entire verification for the correctness of the entered input file

--- main.py
+++ main.py
@@ -5,8 +5,10 @@
 
 import os
 from os import path
+from fpdf import FPDF
 
-def inputFile():
+
+def input_file():
     filepath = input("Enter the full path of the file to convert: ")
     verified = True
     if path.exists(filepath):
@@ -18,14 +20,34 @@ def inputFile():
     else:
         print("Specified filepath is not existing")
         verified = False
-
     return filepath, verified
+
+
+def file_type(filepath):
+    print("The file you entered is a ." + str(filepath.split(".")[1]) + " file.")
+
+
+def create_pdf(filepath):
+    i = 0   # Index to change the line with every new line of the original file
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    with open(filepath, encoding='utf8') as f:  # Open the file to copy
+        for line in f:  # Take a new line of the file
+            pdf.cell(ln=i, h=5.0, align='L', w=0, txt=line, border=0)   # Copy the line to the created PDF file
+            i = i + 1
+    pdf.output(filepath.split(".")[0] + '.pdf', 'F')    # Close PDF file with the information
+
 
 def main(name):
     # Ask for the full path of the file to convert.
-    filepath, verification = inputFile()
+    filepath, verification = input_file()
     while not verification:
-        filepath, verification = inputFile()
+        filepath, verification = input_file()
+    # Determine the type of the file to convert
+    file_type(filepath)
+    # Convert the file to PDF format
+    create_pdf(filepath)
 
 
 # Press the green button in the gutter to run the script.
