ef8607182eeaa865c518fdf01f933aa2834a061c
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-09 19:04:44 +0200

    Push -- able to manage different filepaths in defaults.txt, but not yet able to convert images, which is the next step

--- main.py
+++ main.py
@@ -23,33 +23,43 @@ def user_file():
 
     return filepath, verified
 
-'''
+
 # Line counter for the cases with several lines and files specified in default.txt
 # In developement, need correctly implement an optimized logic on the main flow for this feature
 def file_lines():
     line_count = 0
+    paths = []
     f = open("defaults.txt", "r")
+    # Count of total number of lines
     for line in f:
+        print(line.rstrip("\n"))
         if line != "\n":
             line_count += 1
-    if line_count == 0:
+            paths.append(line)
+    f.close()
+    if line_count == 0:  # Not to be entered in normal conditions; just safety case
         print("No information available about files to read...")
         filepath, verified = user_file()  # User defined file
-    elif line_count == 1:
+    elif line_count == 1:  # Not to be entered in normal conditions; just safety case
         filepath = f.readline()
         verified = True
     else:
         print("Several files indicated in defaults, please select one...")
-        for line in f:
+        for line in paths:
             if line != "\n":
-                print(line)
+                print(line.rstrip("\n"))
                 ans = input("Should I select this one?  (y/n)...   ")
                 if ans == 'y':
                     print("OK")
+                    filepath = line.rstrip("\n")
+                    verified = True
+                    break    # Break here, the file is already selected so no need to go through the rest.
                 else:
                     print()
-    f.close()
-'''
+                    filepath = "No file selected"
+                    verified = False
+    return filepath, verified
+
 
 def input_file():
     # Check if we use the specified file in the defaults.txt file or a new one
@@ -59,16 +69,18 @@ def input_file():
             f = open("defaults.txt", "r")
             filepath = f.readline()
             if filepath.endswith("\n"):
+                filepath, verified = file_lines()
+            else:
                 filepath = filepath.rstrip("\n")
-            verified = True
+                verified = True
             f.close()
-        elif ans == 'n': # new file to be defined by user input
-            filepath, verified = user_file()    # User defined file
+        elif ans == 'n':  # new file to be defined by user input
+            filepath, verified = user_file()  # User defined file
         else:
             print("I do not understand what you want to do. ")
             filepath, verified = user_file()  # User defined file
     else:
-        filepath, verified = user_file()    # User defined file
+        filepath, verified = user_file()  # User defined file
 
     return filepath, verified
 
@@ -78,7 +90,7 @@ def file_type(filepath):
 
 
 def create_pdf(filepath):
-    i = 0   # Index to change the line with every new line of the original file
+    i = 0  # Index to change the line with every new line of the original file
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Arial', 'B', 16)
@@ -86,16 +98,16 @@ def create_pdf(filepath):
         for line in f:  # Take a new line of the file
             if line.startswith('#') or line.startswith('    #'):
                 pdf.set_font('Arial', 'I', 11)
-                pdf.set_text_color(0,150,125)
+                pdf.set_text_color(0, 150, 125)
             elif line.startswith('def'):
                 pdf.set_font('Arial', 'B', 12)
                 pdf.set_text_color(255, 0, 0)
             else:
                 pdf.set_font('Arial', '', 10)
                 pdf.set_text_color(0, 0, 0)
-            pdf.cell(ln=i, h=5.0, align='L', w=0, txt=line, border=0)   # Copy the line to the created PDF file
+            pdf.cell(ln=i, h=5.0, align='L', w=0, txt=line, border=0)  # Copy the line to the created PDF file
             i = i + 1
-    pdf.output(filepath.split(".")[0] + '.pdf', 'F')    # Close PDF file with the information
+    pdf.output(filepath.split(".")[0] + '.pdf', 'F')  # Close PDF file with the information
     pdf.close()
 
 
